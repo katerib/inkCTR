b3fae41459f7834ea6c348455f0aebc91f439f19
Kat <91296845+katerib@users.noreply.github.com>
2023-01-13 16:06:26 -0500

    import csv ; added comments & docstrings to count_mode

--- count.py
+++ count.py
@@ -1,7 +1,10 @@
+import csv
+
 from collections import Counter
 from pypdf import PdfReader
 from helpers import validate
 
+# Add additional custom words to ignore:
 IGNORED = ['chapter', 'about', 'well', 'away', 'because', 'before', 'beneath', 'beside', 'besides', 'between', 'beyond',
            'means', 'down', 'during', 'case', 'front', 'lieu', 'place', 'into', 'inside', 'instead', 'except', 'that',
            'theirs', 'ours', "you're", "i'll", "she'll", "he'll", "will", "they'll", 'near', 'next', 'like', 'this',
@@ -9,12 +12,20 @@ IGNORED = ['chapter', 'about', 'well', 'away', 'because', 'before', 'beneath', '
 
 
 def count_mode():
+    """
+    Reads PDF indicated by user and counts each occurrence of a word throughout entire document. Filters out
+    words from ignored list (IGNORED) and removes punctuation.
+    Users may add additional words to IGNORED in line 6 as needed.
+    Special characters to remove/ignore can be seen and adjusted in line 40. To keep special characters and
+        punctuation, delete or comment out lines 49-42.
+    :return: n/a
+    """
     print("\n<<<< ENTERED WORD COUNT MODE >>>>")
-    pdf_prompt = "Input the name of the PDF to scan: "
+    pdf_prompt = "Input the name of the PDF to scan: "          # get user input
     in_name = input(pdf_prompt)
     pdf_name = validate(in_name, "pdf")
     print("  >> ...searching for PDF...")
-    if not pdf_name:
+    if not pdf_name:                                            # confirm file or path exists
         print(f"ERROR: '{pdf_name}' not found!\nPlease ensure the PDF is in the current directory or provide"
               f" the full path name to the PDF file and try again.")
         return
@@ -23,30 +34,28 @@ def count_mode():
     print("  >> ...processing data... please wait...")
     page_len = len(reader.pages)
     text = ""
-    for count in range(page_len):
+    for count in range(page_len):                               # read data
         page = reader.pages[count]
         page_text = page.extract_text()
         text += page_text
     # clean data
-    for char in '-.,:;!?—"\n':
+    for char in '-.,:;!?—~`()"\n':                              # remove specified punctuation
         text = text.replace(char, ' ')
     text = text.lower()
     words = text.split()
 
-    # validate data and append to new variable
-    c = Counter(words).most_common()
+    c = Counter(words).most_common()                            # count by most common (> freq -> < freq)
     data = []
     for item in c:
         if item[0] in IGNORED or (len(item[0]) <= 3):
             continue
-        data.append(item)
+        data.append(item)                                       # append non-ignored words and words > 3 letters long
 
-    # export to csv
-    csv_name = in_name + '_results.csv'
+    csv_name = in_name + '_results.csv'                         # store .csv filename to store results
     with open(csv_name, 'w', newline='', encoding="utf-8") as results:
         csv_write = csv.writer(results)
-        csv_write.writerow(['WORD', 'FREQUENCY'])
+        csv_write.writerow(['WORD', 'FREQUENCY'])               # write header
         for item in data:
-            csv_write.writerow(item)
-    print(f"Success! {len(data)} unique word counts have been exported to '{csv_name}'!")
+            csv_write.writerow(item)                            # write to csv, note encoding
+    print(f"Success! {len(data)} unique word counts have been exported to '{csv_name}'!")   # print stats to user
     print("<<<< EXITED WORD COUNT MODE >>>>")
